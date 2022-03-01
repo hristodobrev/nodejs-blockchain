eb749ba4a5f89f00f435f45bd934369f200d24a4
hristodobrev <hristo.nankov1@gmail.com>
2022-03-01 08:38:54 +0000

    Removed some console.logs

--- blockchain.js
+++ blockchain.js
@@ -3,42 +3,45 @@ let Block = require('./block');
 const { timingSafeEqual } = require('crypto');
 
 class Blockchain {
-    constructor(genesisBlock, chain, difficulty) {
-        this.genesisBlock = genesisBlock;
-        this.chain = chain;
-        this.difficulty = difficulty;
-        this.blockTime = 1000;
+  constructor(genesisBlock, chain, difficulty) {
+    this.genesisBlock = genesisBlock;
+    this.chain = chain;
+    this.difficulty = difficulty;
+    this.blockTime = 1000;
+  }
+
+  static create(difficulty) {
+    const genesisBlock = new Block(null);
+
+    return new Blockchain(genesisBlock, [genesisBlock], difficulty);
+  }
+
+  addBlock(from, to, amount) {
+    const blockData = { from, to, amount };
+    const lastBlock = this.chain[this.chain.length - 1];
+    const newBlock = new Block(blockData, lastBlock.hash);
+    newBlock.mine(this.difficulty);
+    this.chain.push(newBlock);
+    this.difficulty +=
+      Date.now() - newBlock.timestamp.getTime() > this.blockTime ? -1 : 1;
+  }
+
+  isValid() {
+    if (this.chain.length == 1) return true;
+
+    for (let index = 1; index < this.chain.length; index++) {
+      const currentBlock = this.chain[index];
+      const previousBlock = this.chain[index - 1];
+      if (
+        currentBlock.hash !== calculateHash(currentBlock) ||
+        currentBlock.previousHash !== previousBlock.hash
+      ) {
+        return false;
+      }
     }
 
-    static create(difficulty) {
-        const genesisBlock = new Block(null);
-
-        return new Blockchain(genesisBlock, [genesisBlock], difficulty);
-    }
-
-    addBlock(from, to, amount) {
-        const blockData = { from, to, amount };
-        const lastBlock = this.chain[this.chain.length - 1];
-        const newBlock = new Block(blockData, lastBlock.hash);
-        newBlock.mine(this.difficulty);
-        this.chain.push(newBlock);
-        this.difficulty += (Date.now() - newBlock.timestamp.getTime()) > this.blockTime ? -1 : 1;
-        console.log('Block added', newBlock);
-    }
-
-    isValid() {
-        if(this.chain.length == 1) return true;
-    
-        for (let index = 1; index < this.chain.length; index++) {
-            const currentBlock = this.chain[index];
-            const previousBlock = this.chain[index - 1];
-            if (currentBlock.hash !== calculateHash(currentBlock) || currentBlock.previousHash !== previousBlock.hash) {
-                return false;
-            }
-        }
-    
-        return true;
-    }
+    return true;
+  }
 }
 
 module.exports = Blockchain;
